1341635672d35b810c985b441563f7d5f3c4b8b5
RAGHUKAN <raghu.kanakam@gmail.com>
2025-05-04 18:29:10 +0530

    new

--- src/ex_01_Java_Basics/Helloworld.java
+++ src/ex_01_Java_Basics/Helloworld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basics;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello,I am beginner to the java");
+        System.out.println("Second line added for green");
     }
 }
